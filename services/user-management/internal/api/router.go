858d227e9aa551b7da8a686330c4ad7c5c6e9f71
Raanan Adut <radesigner84@gmail.com>
2024-05-12 23:24:30 +0300

    move routes from main to routes file

--- services/user-management/internal/api/router.go
+++ services/user-management/internal/api/router.go
@@ -1,1 +1,21 @@
 package api
+
+import "github.com/gin-gonic/gin"
+
+func Routers() {
+	router := gin.Default()
+
+	//initial Postgres
+	router.POST("/signup", Signup)
+	router.POST("/login", Login)
+	router.GET("/validate", RequireAuth, Validate)
+	router.POST("/logout", Logout)
+	router.GET("/users", RequireAuth, GetUsers)
+	router.PUT("/users/:id", RequireAuth, UpdateUser)
+	router.POST("/roles", RequireAuth, CreateRole)
+	router.PUT("/roles/:id", RequireAuth, UpdateRole)
+	router.GET("/roles", RequireAuth, GetRoles)
+	router.DELETE("/roles/:id", RequireAuth, DeleteRole)
+	router.PATCH("/roles/recover/:id", RequireAuth, RecoverRole)
+	router.Run()
+}
--- services/user-management/main.go
+++ services/user-management/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"user-management/internal/api"
 	"user-management/internal/initializers"
-
-	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -14,20 +12,5 @@ func init() {
 }
 
 func main() {
-
-	router := gin.Default()
-
-	//initial Postgres
-	router.POST("/signup", api.Signup)
-	router.POST("/login", api.Login)
-	router.GET("/validate", api.RequireAuth, api.Validate)
-	router.POST("/logout", api.Logout)
-	router.GET("/users", api.RequireAuth, api.GetUsers)
-	router.PUT("/users/:id", api.RequireAuth, api.UpdateUser)
-	router.POST("/roles", api.RequireAuth, api.CreateRole)
-	router.PUT("/roles/:id", api.RequireAuth, api.UpdateRole)
-	router.GET("/roles", api.RequireAuth, api.GetRoles)
-	router.DELETE("/roles/:id", api.RequireAuth, api.DeleteRole)
-	router.PATCH("/roles/recover/:id", api.RequireAuth, api.RecoverRole)
-	router.Run()
+	api.Routers()
 }
