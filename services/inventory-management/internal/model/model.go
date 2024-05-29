6b15e34425986b7802d78f3f42ad87421a5dc77a
Raanan Adut <radesigner84@gmail.com>
2024-05-29 21:23:32 +0300

    create consumer to inventory service

--- services/inventory-management/internal/model/model.go
+++ services/inventory-management/internal/model/model.go
@@ -50,6 +50,17 @@ type Supplier struct {
 	Contact     string     `json:"contact"`
 }
 
+type Order struct {
+	ID         uint       `gorm:"primarykey" json:"id"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
+	DeletedAt  *time.Time `gorm:"index" json:"deleted_at,omitempty"`
+	ProductID  uint
+	Quantity   uint
+	CustomerID uint
+	Status     string
+}
+
 type ErrorResponse struct {
 	Error string `json:"message"`
 }
---
+++ services/inventory-management/kafka/kafka_new_order_consumer.go
@@ -0,0 +1,40 @@
+package kafka
+
+import (
+	"context"
+	"inventory-management/internal/initializers"
+	"inventory-management/internal/model"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func ConsumerOrderEvents() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		Topic:    "order-events",
+		GroupID:  "inventory-management-group",
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			panic("could not read message " + err.Error())
+		}
+		log.Printf("received message: %s\n", string(m.Value))
+
+		var order model.Order
+		if results := initializers.DB.First(&order, string(m.Value)); results.Error != nil {
+			log.Printf("failed to find order: %v", results.Error)
+			continue
+		}
+
+		if order.Quantity <= 10 {
+			order.Status = "Ready"
+		} else {
+			order.Status = "Out of Stock"
+		}
+		initializers.DB.Save(&order)
+	}
+}
