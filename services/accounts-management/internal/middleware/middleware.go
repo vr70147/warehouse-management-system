370537754d3ac18ce140f9bf349e1c3c770bb9f3
Raanan Adut <radesigner84@gmail.com>
2024-07-12 22:44:33 +0300

    update JWT middleware with accountID in account service

--- services/accounts-management/internal/middleware/middleware.go
+++ services/accounts-management/internal/middleware/middleware.go
@@ -1,12 +1,16 @@
 package middleware
 
 import (
+	"accounts-management/internal/model"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"gorm.io/gorm"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -25,15 +29,16 @@ func CORSMiddleware() gin.HandlerFunc {
 	}
 }
 
-func AuthMiddleware() gin.HandlerFunc {
+func AuthMiddleware(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenString := c.GetHeader("Authorization")
-
 		if tokenString == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
 			c.Abort()
 			return
 		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -55,7 +60,53 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		c.Set("user_id", claims["user_id"])
+		accountID, ok := claims["account_id"].(string)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			c.Abort()
+			return
+		}
+
+		userID, ok := claims["sub"].(float64)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User ID not found in token claims"})
+			c.Abort()
+			return
+		}
+
+		user, err := fetchUserDetails(uint(userID))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+			c.Abort()
+			return
+		}
+
+		c.Set("account_id", accountID)
+		c.Set("user", user)
+
 		c.Next()
 	}
+}
+
+// fetchUserDetails fetches user details from the user service
+func fetchUserDetails(userID uint) (model.User, error) {
+	var user model.User
+	userServiceURL := os.Getenv("USER_SERVICE_URL")
+
+	resp, err := http.Get(fmt.Sprintf("%s/users/%d", userServiceURL, userID))
+	if err != nil {
+		return user, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return user, fmt.Errorf("failed to fetch user details")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
