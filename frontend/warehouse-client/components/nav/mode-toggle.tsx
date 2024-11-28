549b8700bc599e0f5d2f87f8b6b90c8365f100b3
Raanan Adut <radesigner84@gmail.com>
2024-11-28 22:56:31 +0200

    fix: Fixed Hydration error in the mode toggle

--- frontend/warehouse-client/components/nav/mode-toggle.tsx
+++ frontend/warehouse-client/components/nav/mode-toggle.tsx
@@ -3,8 +3,16 @@ import React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { Button } from '../ui/button';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
   const { theme, setTheme } = useTheme();
   return (
     <Button
