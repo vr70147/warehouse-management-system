e229593da5678758ba1f2f4720510bec696c8f0f
Raanan Adut <radesigner84@gmail.com>
2024-12-13 23:35:20 +0200

    feat: initialize React client with Vite, Tailwind CSS, and shadcn

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+			},
+		},
+	},
+	plugins: [tailwindcssAnimate],
+}
+
